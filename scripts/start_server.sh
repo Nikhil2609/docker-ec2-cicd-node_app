c4959e62587819c4414f04b060587d89bc082d03
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-07-29 18:50:02 +0530

    changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-echo 'before login'
-
 # Pull latest image from ECR
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 677674062026.dkr.ecr.ap-south-1.amazonaws.com
 
-echo 'after login'
-
-echo 'before docker pull'
-
-sudo docker pull 677674062026.dkr.ecr.ap-south-1.amazonaws.com/docker-image-repository:latest
-echo 'after docker pull'
+docker pull 677674062026.dkr.ecr.ap-south-1.amazonaws.com/docker-image-repository:latest
 
 # Stop & remove existing container
-sudo docker stop node-app || true
-sudo docker rm node-app || true
+docker stop node-app || true
+docker rm node-app || true
 
-echo 'before container run'
 # Run new container
-sudo docker run -d -p 3000:3000 --name node-app 677674062026.dkr.ecr.ap-south-1.amazonaws.com/docker-image-repository:latest
-echo 'after container run'
+docker run -d -p 3000:3000 --name node-app 677674062026.dkr.ecr.ap-south-1.amazonaws.com/docker-image-repository:latest
