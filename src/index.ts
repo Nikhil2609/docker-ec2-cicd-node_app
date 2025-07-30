17c7356202e15af2cb48480e01847b585411d3b3
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-07-30 15:35:58 +0530

    index file changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Deploy using codedeploy-agent");
+    res.status(200).send("Changes deployed on 2 EC2 instance");
 });
 
 app.listen(PORT, () => {
