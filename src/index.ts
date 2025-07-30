5594c9e79d09b33f565062bbb95c0dbb8e028754
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-07-30 10:14:26 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello from ECS + farget + Docker + CI-CD Pipeline");
+    res.status(200).send("Hello from ECS + farget + Docker + CI-CD Pipeline v3");
 });
 
 app.listen(PORT, () => {
